37feedbada6eba4b2e3bd78b8114d3dc598cb828
harsh082ip <109286547+harsh082ip@users.noreply.github.com>
2024-01-12 19:36:36 +0530

    Added POST request using Gin

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 
 	"github.com/gin-gonic/gin"
 )
@@ -14,17 +15,35 @@ func main() {
 
 	router := gin.Default()
 
-	router.GET("/getdata", func(c *gin.Context) {
-		// this gin context contains every information about response
-		// and request
-
-		// this is an object of response writer, gin.H is header
-		c.JSON(200, gin.H{
-			"name":      "I am harsh",
-			"Techstack": "golang",
-		})
-	})
+	router.GET("/getdata", getData)
+	router.POST("/postdata", postData)
 
 	fmt.Printf("Starting server at port %v ...", WEBPORT)
 	router.Run(WEBPORT)
+}
+
+func getData(c *gin.Context) {
+	// this gin context contains every information about response
+	// and request
+
+	// this is an object of response writer, gin.H is header
+	c.JSON(200, gin.H{
+		"name":      "I am harsh",
+		"Techstack": "golang",
+	})
+}
+
+func postData(c *gin.Context) {
+	// this gin context contains every information about response
+	// and request
+
+	body := c.Request.Body
+	val, _ := io.ReadAll(body)
+
+	// this is an object of response writer, gin.H is header
+	c.JSON(200, gin.H{
+		"data":      "Hello",
+		"name":      "I am harsh",
+		"body data": string(val),
+	})
 }
