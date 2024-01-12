f72720f452d487df28bedc623f94575bec7d79aa
harsh082ip <109286547+harsh082ip@users.noreply.github.com>
2024-01-12 19:54:34 +0530

    Added getQueryString and getParams method

--- main.go
+++ main.go
@@ -17,7 +17,8 @@ func main() {
 
 	router.GET("/getdata", getData)
 	router.POST("/postdata", postData)
-
+	router.GET("/getquerystring", getQueryString)
+	router.GET("/getparams/:name/:age", getParams)
 	fmt.Printf("Starting server at port %v ...", WEBPORT)
 	router.Run(WEBPORT)
 }
@@ -37,6 +38,7 @@ func postData(c *gin.Context) {
 	// this gin context contains every information about response
 	// and request
 
+	// reading body from Request
 	body := c.Request.Body
 	val, _ := io.ReadAll(body)
 
@@ -45,5 +47,31 @@ func postData(c *gin.Context) {
 		"data":      "Hello",
 		"name":      "I am harsh",
 		"body data": string(val),
+	})
+}
+
+func getQueryString(c *gin.Context) {
+
+	// reading query parameters
+	name := c.Query("name")
+	age := c.Query("age")
+
+	c.JSON(200, gin.H{
+		"data": "This is getQueryString method",
+		"name": name,
+		"age":  age,
+	})
+}
+
+func getParams(c *gin.Context) {
+
+	name := c.Param("name")
+	age := c.Param("age")
+
+	// this is a object of response writer
+	c.JSON(200, gin.H{
+		"data": "This is getParams method",
+		"name": name,
+		"age":  age,
 	})
 }
